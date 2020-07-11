151a15d703ff04dea515f83ce283dbffa1a6a607
Pratyush Shukla <pratyush.11708251@lpu.in>
2020-07-11 18:34:44 +0530

    Initialized Percolation and PercolationStats files

---
+++ 1-percolation/PercolationStats.java
@@ -0,0 +1,39 @@
+/* *****************************************************************************
+ *  Name:              Pratyush Shukla
+ *  Coursera User ID:  hawk.praxs@outlook.com
+ *  Last modified:     11/07/2020
+ **************************************************************************** */
+
+public class PercolationStats {
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        
+    }
+
+}
